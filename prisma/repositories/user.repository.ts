08ae88ba5f2a60cce8ec52e5663db4aa3aff8a8e
thayvitareli <thainyvitareli@hotmail.com>
2025-11-19 21:16:02 -0300

    feat: user Repository

--- prisma/repositories/user.repository.ts
+++ prisma/repositories/user.repository.ts
@@ -1,43 +1,69 @@
 import { Injectable } from '@nestjs/common';
 import { user, Prisma } from '@prisma/client';
 import { PrismaService } from 'prisma/prisma.service';
-
-@Injectable()
-export class UserRepository {
-  constructor(private prisma: PrismaService) {}
-
-  async create(data: Prisma.userCreateInput): Promise<user> {
-    return this.prisma.user.create({
-      data,
-    });
-  }
-
-  async findOne(where: Prisma.userWhereInput): Promise<user> {
-    return this.prisma.user.findFirst({
-      where,
-    });
-  }
-
-  async update(
-    where: Prisma.userWhereUniqueInput,
-    data: Prisma.userUpdateInput,
-  ): Promise<user> {
-    return this.prisma.user.update({
-      where,
-      data,
-    });
-  }
-}
-
+import { User } from 'src/modules/users/entities/user.entity';
 
 export interface IUserRepository {
-
-  create(data: Prisma.userCreateInput): Promise<user>
-
-   findOne(where: Prisma.userWhereInput): Promise<user>
-
-   update(
-    where: Prisma.userWhereUniqueInput,
-    data: Prisma.userUpdateInput,
-  ): Promise<user> 
+  create(user: User): Promise<void>;
+  findByEmail(email: string): Promise<User | null>;
+  findById(id: string): Promise<User | null>;
+  update(user: User): Promise<void>;
+  delete(id: string): Promise<void>;
 }
+
+
+@Injectable()
+export class UserRepository implements IUserRepository {
+  constructor(private prisma: PrismaService) {}
+ 
+
+  async create(user: User): Promise<void> {
+    await this.prisma.user.create({
+      data: {
+        email: user.email,
+        name: user.name, 
+        avatar: user.avatar,
+        password: user.password,
+      },
+    });
+  }
+
+  async findById(id: string): Promise<User | null>{
+    const data = await this.prisma.user.findFirst({
+      where: {id}
+    })
+
+    return data ? new User(data) : null
+
+    }
+
+
+  async findByEmail(email: string): Promise<User> {
+    const data = await this.prisma.user.findFirst({
+      where: { email }
+    });
+
+    return data ? new User(data) : null;
+  }
+
+  async update(user: User): Promise<void> {
+    await this.prisma.user.update({
+      where: { id: user.id },
+      data: {
+        name: user.name,
+        avatar: user.avatar,
+        password: user.password,
+      },
+    });
+  }
+
+   async delete(id: string): Promise<void> {
+    await this.prisma.user.delete({
+      where: { id}
+    })
+  }
+
+}
+
+
+
