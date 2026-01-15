b16ba022c2471c800edb934ce30aa7d249a8179e
thayvitareli <thainyvitareli@hotmail.com>
2026-01-15 18:45:50 -0300

    feat: category model

---
+++ src/modules/categories/domain/model/category.model.ts
@@ -0,0 +1,17 @@
+import { BaseEntity } from "src/shared/entity/base-entity";
+
+export class Category extends BaseEntity {
+  public name: string;
+  public image_uri: string;
+
+  constructor(props: { id?: string; name: string; image_uri: string; created_at?: Date }) {
+    super({ id: props.id, created_at: props.created_at });
+    this.name = props.name;
+    this.image_uri = props.image_uri;
+  }
+
+  public rename(newName: string) {
+    if (!newName.trim()) throw new Error("Nome da categoria não pode ser vazio.");
+    this.name = newName;
+  }
+}
