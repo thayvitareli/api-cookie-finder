b332d7f1a3304c61c19b39afd06ef16e60a9c93f
thayvitareli <thainyvitareli@hotmail.com>
2026-02-07 13:11:57 -0300

    refactor: add code prop on the category model

--- src/modules/categories/domain/model/category.model.ts
+++ src/modules/categories/domain/model/category.model.ts
@@ -1,17 +1,26 @@
-import { BaseEntity } from "src/shared/entity/base-entity";
+import { BaseEntity } from 'src/shared/entity/base-entity';
 
 export class Category extends BaseEntity {
   public name: string;
   public image_uri: string;
+  public code: string;
 
-  constructor(props: { id?: string; name: string; image_uri: string; created_at?: Date }) {
+  constructor(props: {
+    id?: string;
+    name: string;
+    image_uri: string;
+    code: string;
+    created_at?: Date;
+  }) {
     super({ id: props.id, created_at: props.created_at });
     this.name = props.name;
     this.image_uri = props.image_uri;
+    this.code = props.code;
   }
 
   public rename(newName: string) {
-    if (!newName.trim()) throw new Error("Nome da categoria não pode ser vazio.");
+    if (!newName.trim())
+      throw new Error('Nome da categoria não pode ser vazio.');
     this.name = newName;
   }
 }
