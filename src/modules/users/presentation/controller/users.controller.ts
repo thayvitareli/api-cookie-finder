b874efae59d0b53e7ff48f6f46f4be4c546dccc9
thayvitareli <thainyvitareli@hotmail.com>
2026-03-05 19:03:36 -0300

    feat: endpoint follow and unfollow user

--- src/modules/users/presentation/controller/users.controller.ts
+++ src/modules/users/presentation/controller/users.controller.ts
@@ -1,14 +1,19 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, UseGuards, Request, Delete } from '@nestjs/common';
 import { Public } from '../../../../shared/decorator/public.decorator';
 import { RegisterUserUseCase } from '../../use-cases/register-user.use-case';
 import { CreateUserDto } from '../dto/create-user.dto';
 import { GetUserProfileUseCase } from '../../use-cases/get-user-profile.use-case';
+import { FollowUserUseCase } from '../../use-cases/follow-user.use-case';
+import { UnfollowUserUseCase } from '../../use-cases/unfollow-user.use-case';
+import { JwtAuthGuard } from '../../../auth/guard/jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(
     private readonly registerUser: RegisterUserUseCase,
     private readonly getUserProfileUseCase: GetUserProfileUseCase,
+    private readonly followUserUseCase: FollowUserUseCase,
+    private readonly unfollowUserUseCase: UnfollowUserUseCase,
   ) {}
 
   @Public()
@@ -21,5 +26,19 @@ export class UsersController {
   @Get(':id')
   getProfile(@Param('id') id: string) {
     return this.getUserProfileUseCase.execute(id);
+  }
+
+  @Post(':id/follow')
+  @UseGuards(JwtAuthGuard)
+  follow(@Param('id') id: string, @Request() req: any) {
+    const userId = req.user.userId;
+    return this.followUserUseCase.execute(id, userId);
+  }
+
+  @Delete(':id/follow')
+  @UseGuards(JwtAuthGuard)
+  unfollow(@Param('id') id: string, @Request() req: any) {
+    const userId = req.user.userId;
+    return this.unfollowUserUseCase.execute(id, userId);
   }
 }
